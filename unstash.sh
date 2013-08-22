f45850556b535f311e64dfa0b2a77d380e0357dc
Jonas Abreu <jonas@vidageek.net>
2013-08-22 14:05:10 -0300

    mudando pasta porque o repo do hudson é meio estranho

--- unstash.sh
+++ unstash.sh
@@ -1,5 +1,11 @@
 #! /bin/bash 
 
+( [ ! -d "~/blog-vidageek" ] ) && git clone git@github.com:~/blog-vidageek
+
+cd ~/blog-vidageek
+
+git pull origin master
+
 POST=`ls _stash/ | head -n 1`;
 
 test -z "$POST" && exit 1
@@ -7,8 +13,6 @@ test -z "$POST" && exit 1
 TODAY=`date +"%Y-%m-%d"`
 
 NOME_FINAL=$TODAY`echo $POST | sed -E "s/^[0-9]+//"`
-
-git checkout master
 
 mv _stash/$POST _posts/$NOME_FINAL
 
