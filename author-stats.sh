4f77a27f10ff9d35dd334587525e266aadc7de05
Jonas Abreu <jonas@vidageek.net>
2013-08-24 17:50:30 -0300

    modificado script para contar também posts na stash

--- author-stats.sh
+++ author-stats.sh
@@ -1,3 +1,3 @@
 #! /bin/bash 
 
-grep -ri "author:" _posts | cut -d ":" -f 3 | sort | uniq -c | sort
+grep "author:" _posts/* _stash/* | grep -v "_stash/new" | cut -d ":" -f 3 | sort | uniq -c | sort
